3d12e8c697677ae47ca4c14c5d65e8c224db8d27
itsnikhil28 <itsnikhil28@gmail.com>
2025-04-05 14:31:55 +0530

    Changes : Scroll To Top Page added

--- src/Layout/Protected-layout.tsx
+++ src/Layout/Protected-layout.tsx
@@ -1,11 +1,13 @@
 import Container from "@/components/Container";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
+import ScrollToTop from "@/components/ScrollToTop";
 import { Outlet } from "react-router-dom";
 
 export default function ProtectedLayout() {
     return (
         <div className='flex flex-col h-screen'>
+            <ScrollToTop />
             <Header />
             <Container className="grow">
                 <main className="grow"></main>
--- src/Layout/Public-layout.tsx
+++ src/Layout/Public-layout.tsx
@@ -1,11 +1,13 @@
 import Footer from '@/components/Footer'
 import Header from '@/components/Header'
+import ScrollToTop from '@/components/ScrollToTop'
 import AuthHandler from '@/handlers/Auth-handler'
 import { Outlet } from 'react-router-dom'
 
 export default function PublicLayout() {
     return (
         <div className='w-full'>
+            <ScrollToTop />
 
             <AuthHandler />
             <Header />
